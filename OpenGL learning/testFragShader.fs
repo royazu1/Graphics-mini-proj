7f24b65bc0058738c5fb9dfd4ec173e2748dade5
lifeOnHack <ilayl9991@gmal.com>
2024-06-18 13:21:05 +0300

    save

--- OpenGL learning/testFragShader.fs
+++ OpenGL learning/testFragShader.fs
@@ -1,9 +1,35 @@
 #version 330 core
 out vec4 fragmentColor;
+
 in vec3 vertexColor;
+//light
+in vec3 FragPos;
+in vec3 Normal;
 
-
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+//
 
 void main() {
-	fragmentColor= vec4(vertexColor,1.0f);
+	// Ambient
+    float ambientStrength = 0.15;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Specular
+    float specularStrength = 0.3;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+	fragmentColor= vec4(result,1.0f);
 }
--- OpenGL learning/testVertexShader.vs
+++ OpenGL learning/testVertexShader.vs
@@ -2,14 +2,22 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 layout (location = 2) in vec3 normal;
-uniform mat4 projectionMatrix;
-uniform mat4 camTransMatrix;
+layout(location = 3) in vec3 aNormal;
+uniform mat4 projectionMatrix;//projection
+uniform mat4 camTransMatrix;//view
+//light
+uniform mat4 model;
+//
 out vec3 vertexColor;
+out vec3 Normal;
 
 uniform vec3 lightDir;
 
 
 void main() {
+	FragPos = vec3(model * vec4(position, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aNormal;
+
 	gl_Position= projectionMatrix * camTransMatrix * vec4(position, 1.0f);
-	vertexColor=color;
+	vertexColor=color;	
 }
